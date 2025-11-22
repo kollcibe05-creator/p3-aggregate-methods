8fbc07547dfaaee94160eb95edc592cc7b8b9d92
Collins Kibet <kollcibe05@gmail.com>
2025-11-22 12:23:52 +0300

    aggregate methods

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -14,6 +14,8 @@ class Student:
 
     def get_enrollments(self):
         return self._enrollments.copy()
+    def course_content(self):           ##counts the number of student's courses
+        return len(self._enrollments)    
 
 class Course:
     def __init__(self, title):
@@ -29,6 +31,13 @@ class Course:
 
     def get_enrollments(self):
         return self._enrollments.copy()
+    ##Assumption ~ calculating student average grade
+    # def aggregate_average_grade(self):
+    #     total_grades = sum(self._grades.values())
+    #     num_courses = len(self._grades)
+    #     average_grade = total_grades/num_courses
+    #     return average_grade
+
 
 
 class Enrollment:
@@ -45,3 +54,10 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+    @classmethod    #to establish how many enrollments were done in a specific day
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) +1
+        return enrollment_count
